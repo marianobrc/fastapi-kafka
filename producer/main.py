e7e9c106c667ba0a388b353f6326080bad369f3d
marianobrc <marianomartinez84@gmail.com>
2022-09-26 18:55:17 -0300

    Implement a very simple producer

---
+++ producer/main.py
@@ -0,0 +1,60 @@
+import json
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+# produce json messages
+producer = KafkaProducer(
+    bootstrap_servers=['broker:9092'],
+    value_serializer=lambda m: json.dumps(m).encode('utf-8')
+)
+producer.send('geo-locations', {'latitude': '41.1335', 'longitude': '71.3103'})
+
+# # Asynchronous by default
+# future = producer.send('my-topic', b'raw_bytes')
+#
+# # Block for 'synchronous' sends
+# try:
+#     record_metadata = future.get(timeout=10)
+# except KafkaError:
+#     # Decide what to do if produce request failed...
+#     log.exception()
+#     pass
+#
+# # Successful result returns assigned partition and offset
+# print (record_metadata.topic)
+# print (record_metadata.partition)
+# print (record_metadata.offset)
+#
+# # produce keyed messages to enable hashed partitioning
+# producer.send('my-topic', key=b'foo', value=b'bar')
+#
+# # encode objects via msgpack
+# producer = KafkaProducer(value_serializer=msgpack.dumps)
+# producer.send('msgpack-topic', {'key': 'value'})
+
+
+
+# produce asynchronously
+# for _ in range(100):
+#     producer.send('my-topic', b'msg')
+#
+#
+# def on_send_success(record_metadata):
+#     print(record_metadata.topic)
+#     print(record_metadata.partition)
+#     print(record_metadata.offset)
+#
+#
+# def on_send_error(excp):
+#     log.error('I am an errback', exc_info=excp)
+#     # handle exception
+#
+#
+# # produce asynchronously with callbacks
+# producer.send('my-topic', b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
+#
+# # block until all async messages are sent
+# producer.flush()
+#
+# # configure multiple retries
+# producer = KafkaProducer(retries=5)
