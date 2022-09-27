7cedaa5b6a9a991a347306bcb498751a7cda35c3
marianobrc <marianomartinez84@gmail.com>
2022-09-27 13:29:42 -0300

    Add a basic consumer

---
+++ consumer/main.py
@@ -0,0 +1,22 @@
+import json
+from kafka import KafkaConsumer
+
+
+# produce json messages
+print("Connecting to the broker..")
+consumer = KafkaConsumer(
+    topic='geo-locations',
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=lambda m: json.dumps(m).encode('utf-8'),
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='location-consumers',
+)
+print("Consuming messages..")
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                         message.offset, message.key,
+                                         message.value))
+print("No more messages.")
