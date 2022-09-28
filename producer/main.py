ec62f1b7ccd037ec56a9d75ea5ce469271dd8566
marianobrc <marianomartinez84@gmail.com>
2022-09-28 18:51:41 -0300

    Build an API to send location messages

--- producer/main.py
+++ producer/main.py
@@ -1,6 +1,8 @@
 import json
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
+from fastapi import FastAPI
+from models import Location
+
 
 # produce json messages
 print("Connecting to the broker..")
@@ -8,61 +10,23 @@ producer = KafkaProducer(
     bootstrap_servers=['kafka:9092'],
     value_serializer=lambda m: json.dumps(m).encode('utf-8')
 )
-print("Sending a message..")
-producer.send(
-    topic='geo-locations',
-    value={'latitude': '41.1335', 'longitude': '71.3103'}
-)
-# block until all async messages are sent
-producer.flush()
-print("Message sent successfully.")
 
-# # Asynchronous by default
-# future = producer.send('my-topic', b'raw_bytes')
-#
-# # Block for 'synchronous' sends
-# try:
-#     record_metadata = future.get(timeout=10)
-# except KafkaError:
-#     # Decide what to do if produce request failed...
-#     log.exception()
-#     pass
-#
-# # Successful result returns assigned partition and offset
-# print (record_metadata.topic)
-# print (record_metadata.partition)
-# print (record_metadata.offset)
-#
-# # produce keyed messages to enable hashed partitioning
-# producer.send('my-topic', key=b'foo', value=b'bar')
-#
-# # encode objects via msgpack
-# producer = KafkaProducer(value_serializer=msgpack.dumps)
-# producer.send('msgpack-topic', {'key': 'value'})
+app = FastAPI()
 
 
+@app.get("/status/")
+async def root():
+    return {"status": "ok"}
 
-# produce asynchronously
-# for _ in range(100):
-#     producer.send('my-topic', b'msg')
-#
-#
-# def on_send_success(record_metadata):
-#     print(record_metadata.topic)
-#     print(record_metadata.partition)
-#     print(record_metadata.offset)
-#
-#
-# def on_send_error(excp):
-#     log.error('I am an errback', exc_info=excp)
-#     # handle exception
-#
-#
-# # produce asynchronously with callbacks
-# producer.send('my-topic', b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
-#
-# # block until all async messages are sent
-# producer.flush()
-#
-# # configure multiple retries
-# producer = KafkaProducer(retries=5)
+
+@app.put("/locations/")
+async def send_location(loc: Location):
+    print("Sending a message..")
+    message_dict = loc.dict(exclude_unset=True)
+    producer.send(
+        topic='geo-locations',
+        value=message_dict
+    )
+    print("Message sent successfully.")
+    return {"status": "Location sent successfully"}
+
